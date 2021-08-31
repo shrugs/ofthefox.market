d0b786c6577702c04997ab080e7cb919f734b0db
Matt Condon <mattgcondon@gmail.com>
2021-08-31 17:32:08 -0500

    fix: filter out auctions?

--- utils/robes.ts
+++ utils/robes.ts
@@ -31,7 +31,11 @@ export const fetchRobes = async () => {
   const data = await pMap(chunked, fetchRobePage, { concurrency: 2 })
   const mapped = flatten(data)
     .filter((d) => {
-      return d.sell_orders && d.sell_orders.length > 0
+      return (
+        d.sell_orders &&
+        d.sell_orders.length > 0 &&
+        d.sell_orders[0].payment_token_contract.symbol == 'ETH'
+      )
     })
     .map((a: Asset): RobeInfo => {
       return {
@@ -41,7 +45,7 @@ export const fetchRobes = async () => {
             BigNumber.from(a.sell_orders[0].current_price.split('.')[0]),
           ),
         ),
-        url: a.permalink + '?ref=0xfb843f8c4992efdb6b42349c35f025ca55742d33',
+        url: a.permalink + '?ref=0xEC6d36A487d85CF562B7b8464CE8dc60637362AC',
         svg: a.image_url,
       }
     })
